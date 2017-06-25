fa95cd6b71a7b004c9bff9c84006c30ebf379ad4
Julien Garcia Gonzalez <julien.garciagonzalez@lampiris.be>
2017-06-25 07:46:11 +0200

    fix shebang in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+#!/bin/bash +eu
+
 sudo /opt/google-cloud-sdk/bin/gcloud docker -- push us.gcr.io/${PROJECT_NAME}/${PROJECT_NAME}
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 kubectl patch deployment ${PROJECT_NAME} -p '{"spec":{"template":{"spec":{"containers":[{"name":"${PROJECT_NAME}","image":"us.gcr.io/'"${PROJECT_NAME}"'/'"${PROJECT_NAME}"':'"$CIRCLE_SHA1"'"}]}}}}'
