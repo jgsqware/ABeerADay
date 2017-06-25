72b5ced0b57bd2291a87bd4a355e7774c547c007
Julien Garcia Gonzalez <julien.garciagonzalez@lampiris.be>
2017-06-25 11:43:16 +0200

    update permission for kubectl

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ docker login -u juliengarcia -p ${DOCKER_PASSWORD} -e garciagonzalez.julien@gmai
 docker push quay.io/juliengarcia/${PROJECT_NAME}:$CIRCLE_SHA1
 docker push quay.io/juliengarcia/${PROJECT_NAME}:latest
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-sudo /opt/google-cloud-sdk/bin/gcloud --quiet container clusters get-credentials $CLUSTER_NAME
+sudo chown -R $USER /home/ubuntu/.config
 kubectl patch deployment ${PROJECT_NAME} -p '{"spec":{"template":{"spec":{"containers":[{"name":"${PROJECT_NAME}","image":"quay.io/juliengarcia/'"${PROJECT_NAME}"':'"$CIRCLE_SHA1"'"}]}}}}'
