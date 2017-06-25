a2aab94b3ebf7c00895cfd7514c2370202e5a569
Julien Garcia Gonzalez <julien.garciagonzalez@lampiris.be>
2017-06-25 11:28:48 +0200

    add repo quay name

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash +eu
 
 docker login -u juliengarcia -p ${DOCKER_PASSWORD} -e garciagonzalez.julien@gmail.com quay.io
-docker push quay.io/${PROJECT_ID}/${PROJECT_NAME}:$CIRCLE_SHA1
-docker push quay.io/${PROJECT_ID}/${PROJECT_NAME}:latest
+docker push quay.io/juliengarcia/${PROJECT_NAME}:$CIRCLE_SHA1
+docker push quay.io/juliengarcia/${PROJECT_NAME}:latest
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-kubectl patch deployment ${PROJECT_NAME} -p '{"spec":{"template":{"spec":{"containers":[{"name":"${PROJECT_NAME}","image":"quay.io/'"${PROJECT_ID}"'/'"${PROJECT_NAME}"':'"$CIRCLE_SHA1"'"}]}}}}'
+sudo /opt/google-cloud-sdk/bin/gcloud --quiet container clusters get-credentials $CLUSTER_NAME
+kubectl patch deployment ${PROJECT_NAME} -p '{"spec":{"template":{"spec":{"containers":[{"name":"${PROJECT_NAME}","image":"quay.io/juliengarcia/'"${PROJECT_NAME}"':'"$CIRCLE_SHA1"'"}]}}}}'
