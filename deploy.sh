b6af1514b89e36cb3cbf8f4400b6876fbf44ab81
Julien Garcia Gonzalez <julien.garciagonzalez@lampiris.be>
2017-06-25 11:23:06 +0200

    add email for circleci

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash +eu
 
-docker login -u juliengarcia -p ${DOCKER_PASSWORD} quay.io
+docker login -u juliengarcia -p ${DOCKER_PASSWORD} -e garciagonzalez.julien@gmail.com quay.io
 docker push quay.io/${PROJECT_ID}/${PROJECT_NAME}:$CIRCLE_SHA1
 docker push quay.io/${PROJECT_ID}/${PROJECT_NAME}:latest
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
