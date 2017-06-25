af923da77cbbaf3090582fc324b429cd506be3b3
Julien Garcia Gonzalez <julien.garciagonzalez@lampiris.be>
2017-06-25 07:57:58 +0200

    fix project id for docker tag

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash +eu
 
-sudo /opt/google-cloud-sdk/bin/gcloud docker -- push us.gcr.io/${PROJECT_NAME}/${PROJECT_NAME}
+sudo /opt/google-cloud-sdk/bin/gcloud docker -- push us.gcr.io/${PROJECT_ID}/${PROJECT_NAME}
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-kubectl patch deployment ${PROJECT_NAME} -p '{"spec":{"template":{"spec":{"containers":[{"name":"${PROJECT_NAME}","image":"us.gcr.io/'"${PROJECT_NAME}"'/'"${PROJECT_NAME}"':'"$CIRCLE_SHA1"'"}]}}}}'
+kubectl patch deployment ${PROJECT_NAME} -p '{"spec":{"template":{"spec":{"containers":[{"name":"${PROJECT_NAME}","image":"us.gcr.io/'"${PROJECT_ID}"'/'"${PROJECT_NAME}"':'"$CIRCLE_SHA1"'"}]}}}}'
