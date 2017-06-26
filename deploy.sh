6c2d94ca60d5947dfd0d21b4861fe0d9dde4856b
Julien Garcia Gonzalez <julien.garciagonzalez@lampiris.be>
2017-06-26 13:11:33 +0200

    update permission for kubectl

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash +eu
 
 docker login -u jgsqware -p ${DOCKER_PASSWORD} -e " " quay.io
-docker push quay.io/juliengarcia/${PROJECT_NAME}:$CIRCLE_SHA1
-docker push quay.io/juliengarcia/${PROJECT_NAME}:latest
+docker tag quay.io/jgsqware/${PROJECT_NAME}:$(git log --format="%H" -n 1) quay.io/jgsqware/${PROJECT_NAME}:latest
+docker push quay.io/jgsqware/${PROJECT_NAME}:$(git log --format="%H" -n 1)
+docker push quay.io/jgsqware/${PROJECT_NAME}:latest
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 sudo chown -R $USER /home/ubuntu/.config
-kubectl patch deployment ${PROJECT_NAME} -p '{"spec":{"template":{"spec":{"containers":[{"name":"${PROJECT_NAME}","image":"quay.io/juliengarcia/'"${PROJECT_NAME}"':'"$CIRCLE_SHA1"'"}]}}}}'
+kubectl apply -f kubernetes/deployment.yaml
