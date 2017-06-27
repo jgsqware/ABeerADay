58c42992089f222048e7cfafdc480c222c361735
Julien Garcia Gonzalez <julien.garciagonzalez@lampiris.be>
2017-06-27 22:29:43 +0200

    clean circleci

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,7 @@
-#!/bin/bash +eu
+#!/bin/bash 
+set -euo pipefail
 
-# docker login -u jgsqware -p ${DOCKER_PASSWORD} -e " " quay.io
-# docker tag quay.io/jgsqware/${PROJECT_NAME}:$(git log --format="%H" -n 1) quay.io/jgsqware/${PROJECT_NAME}:latest
-# docker push quay.io/jgsqware/${PROJECT_NAME}:$(git log --format="%H" -n 1)
-# docker push quay.io/jgsqware/${PROJECT_NAME}:latest
-# sudo ./clairctl analyze quay.io/jgsqware/${PROJECT_NAME}:$(git log --format="%H" -n 1)
-sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
+# sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 sudo chown -R $USER /home/ubuntu/.config
 sed -i 's/{{SHA1}}/'$(git log --format="%H" -n 1)'/' kubernetes/deployment.yaml
 kubectl apply -f kubernetes/deployment.yaml
