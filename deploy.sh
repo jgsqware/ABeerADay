e11094cc115edea072af477a214511bcb00aac23
Julien Garcia Gonzalez <julien.garciagonzalez@lampiris.be>
2017-06-27 22:45:41 +0200

    clean circleci

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 set -euo pipefail
 
-# sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
+sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 sudo chown -R $USER /home/ubuntu/.config
 sed -i 's/{{SHA1}}/'$(git log --format="%H" -n 1)'/' kubernetes/deployment.yaml
 kubectl apply -f kubernetes/deployment.yaml
