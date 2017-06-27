b9ef9dd105e9e0ff04e6746266c230f3dde465ed
Julien Garcia Gonzalez <julien.garciagonzalez@lampiris.be>
2017-06-27 10:42:01 +0200

    use  clairctl after push

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ docker login -u jgsqware -p ${DOCKER_PASSWORD} -e " " quay.io
 docker tag quay.io/jgsqware/${PROJECT_NAME}:$(git log --format="%H" -n 1) quay.io/jgsqware/${PROJECT_NAME}:latest
 docker push quay.io/jgsqware/${PROJECT_NAME}:$(git log --format="%H" -n 1)
 docker push quay.io/jgsqware/${PROJECT_NAME}:latest
+sudo ./clairctl analyze quay.io/jgsqware/${PROJECT_NAME}:$(git log --format="%H" -n 1)
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 sudo chown -R $USER /home/ubuntu/.config
 sed -i 's/{{SHA1}}/'$(git log --format="%H" -n 1)'/' kubernetes/deployment.yaml
